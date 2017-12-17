b50c92463c9282e7d6fd3d4962c5c21386fcc856
Lucas Shanker <lshanke@purdue.edu>
2017-12-17 10:21:54 -0600

    Implement food model

--- app/src/main/java/com/lshan/boilerfaves/Models/FoodModel.java
+++ app/src/main/java/com/lshan/boilerfaves/Models/FoodModel.java
@@ -1,8 +1,41 @@
 package com.lshan.boilerfaves.Models;
 
+import android.support.annotation.NonNull;
+
 /**
  * Created by lshan on 12/16/2017.
  */
 
-public class FoodModel {
+public class FoodModel implements Comparable{
+    public String name;
+    public boolean isVegetarian;
+
+
+    public FoodModel(String name, boolean isVegetarian){
+        this.name = name;
+        this.isVegetarian = isVegetarian;
+    }
+
+
+    @Override
+    public boolean equals(Object obj){
+        if (!FoodModel.class.isAssignableFrom(obj.getClass())) {
+            return false;
+        }
+
+        FoodModel other = (FoodModel) obj;
+        if(other.name.equals(this.name)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
+    @Override
+    public int compareTo(@NonNull Object o) {
+        FoodModel other = (FoodModel)o;
+
+        return this.name.compareTo(other.name);
+    }
 }
