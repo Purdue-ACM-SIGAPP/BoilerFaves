231c98c30012a4e98cae84b17a28b7c0dba2d39e
Lucas Shanker <lshanke@purdue.edu>
2018-01-27 18:56:38 -0500

    Call api again on onResume

--- app/src/main/java/com/lshan/boilerfaves/Activities/MainActivity.java
+++ app/src/main/java/com/lshan/boilerfaves/Activities/MainActivity.java
@@ -87,6 +87,7 @@ public class MainActivity extends AppCompatActivity{
             startAdaptor(faveList);
         }
 
+        new MenuRetrievalTask(context, mainRecyclerView).execute();
     }
 
 
