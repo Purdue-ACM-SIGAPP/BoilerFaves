548e4088a6e9041981839fd7d60bc1174cdaea5a
Christopher Rosenblatt <rosench13@gmail.com>
2018-02-06 19:44:26 -0500

    commented out fab thing

--- app/src/main/java/com/lshan/boilerfaves/Activities/NotificationActivity.java
+++ app/src/main/java/com/lshan/boilerfaves/Activities/NotificationActivity.java
@@ -18,7 +18,7 @@ public class NotificationActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,6 +26,7 @@ public class NotificationActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        */
     }
 
 }
