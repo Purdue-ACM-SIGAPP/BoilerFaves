1b276b8e9fa7b42c82d09658592bb9519d92aa4f
Kevin Vosburgh <kvosbur@purdue.edu>
2018-02-13 20:01:40 -0500

    Added time picker.

--- app/src/main/java/com/lshan/boilerfaves/Activities/NotificationActivity.java
+++ app/src/main/java/com/lshan/boilerfaves/Activities/NotificationActivity.java
@@ -4,12 +4,31 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SwitchCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.TimePicker;
 
 import com.lshan.boilerfaves.R;
 
+import java.sql.SQLOutput;
+
+import butterknife.BindDrawable;
+import butterknife.BindView;
+import butterknife.OnCheckedChanged;
+import butterknife.OnClick;
+
 public class NotificationActivity extends AppCompatActivity {
+
+
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.breakfastSwitch)
+    SwitchCompat breakfastSwitch;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +36,10 @@ public class NotificationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_notification);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        TimePicker timePicker = findViewById(R.id.timePicker);
+        timePicker.setVisibility(View.INVISIBLE);
 
         /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +50,18 @@ public class NotificationActivity extends AppCompatActivity {
             }
         });
         */
+
+    }
+
+    @OnClick(R.id.breakfastSwitch)
+    public void onCheckedChanged(CompoundButton buttonView) {
+        System.out.println("got here!!!!");
+        if (false) {
+            //if 'isChecked' is true do whatever you need...
+            System.out.println("Breakfast is checked!!!!!!!");
+        } else {
+            System.out.println("Breakfast is unchecked!!!!");
+        }
     }
 
 }
