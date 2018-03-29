27965e164ac7485cba0a4593d5e48fabf75d7a6e
Lucas Shanker <lshanke@purdue.edu>
2018-03-29 19:52:51 -0400

    Dismiss notification after you click it

--- app/src/main/java/com/lshan/boilerfaves/Activities/MainActivity.java
+++ app/src/main/java/com/lshan/boilerfaves/Activities/MainActivity.java
@@ -1,6 +1,7 @@
 package com.lshan.boilerfaves.Activities;
 
 import android.app.AlarmManager;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -53,6 +54,14 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Dismiss notification if the user clicked a notification to get here
+        Bundle b = getIntent().getExtras();
+        if(b != null){
+            int id = b.getInt("notificationID", 0);
+            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            notificationManager.cancel(id);
+        }
 
         ButterKnife.bind(this);
 
