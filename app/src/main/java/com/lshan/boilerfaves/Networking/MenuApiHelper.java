d9e7f7a92702a47870e4f42f0f1c4d7b22890e3d
Lucas Shanker <lshanke@purdue.edu>
2018-04-05 20:31:22 -0400

    delete some unused dependencies

--- app/src/main/java/com/lshan/boilerfaves/Networking/MenuApiHelper.java
+++ app/src/main/java/com/lshan/boilerfaves/Networking/MenuApiHelper.java
@@ -1,14 +1,8 @@
 package com.lshan.boilerfaves.Networking;
 
-import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
-
-import org.simpleframework.xml.convert.AnnotationStrategy;
-import org.simpleframework.xml.core.Persister;
-
-import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
+
 
 /**
  * Created by lshan on 12/17/2017.
